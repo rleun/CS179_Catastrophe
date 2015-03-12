2f4cc7a773b158522d7d6ffb72899cda5a120707
jonpang <jpang005@ucr.edu>
2015-03-11 18:07:58 -0700

    DoorScript

---
+++ Assets/Scripts/DoorScript.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+//This function will control when the door opens 
+//The door will open when a certain enemy has been defeated
+public class DoorScript : MonoBehaviour {
+	//Trigger is the enemy GameObject
+	public GameObject Trigger;
+	EnemyHealth enemyHealth;
+	Animator anim;
+	// Use this for initialization
+	void Start () 
+	{
+		anim = GetComponent<Animator> ();
+		enemyHealth = Trigger.GetComponent<EnemyHealth> ();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (enemyHealth.currentHealth <= 0) {
+			anim.SetBool("EnemyDead", true);
+		}
+	}
+}

4b5b6f3427ebae4ed16828bbb8c6971071e13ab3
jonpang <jpang005@ucr.edu>
2015-03-12 19:44:48 -0700

    New Model, Look, Relocated laserbolt

---
+++ Assets/Scripts/LookAt.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LookAt : MonoBehaviour {
+	public Transform target;
+	//NavMeshAgent nav;
+	// Update is called once per frame
+	void Start ()
+	{
+		//nav = transform.GetComponent<NavMeshAgent> ();
+	}
+	void Update () 
+	{
+		//	nav.SetDestination (target.position);
+		transform.LookAt (target);
+	}
+}

f7f7cf5c81fec9f783d333a606b7cf35e3b71a28
raymond <weiwenliang1990@gmail.com>
2015-03-13 00:40:00 -0700

    Add helicopter dead animation and fixed robot health bar

---
+++ Assets/Scripts/HelicopterScripts/HelicopterAnimatorScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicopterAnimatorScript : MonoBehaviour {
+
+	EnemyAI EnemyAIScript;
+	Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		EnemyAIScript = GetComponent<EnemyAI> ();
+		anim = GetComponent<Animator> ();
+
+		anim.SetBool ("Take-Off", false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if(EnemyAIScript.currentHealth > 0)
+		{
+			if(EnemyAIScript.PlayerWithInRange)
+			{
+				anim.SetBool ("Take-Off", true);
+			}
+		}
+		else
+		{
+			anim.SetTrigger("Death");
+		}
+	}
+}

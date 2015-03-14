2bf603f834f2e5069636179cf8c22c85fbf86ec7
cto002 <conniet90964@gmail.com>
2015-03-13 20:30:09 -0700

    Added the Main Menu, Instruction Menu, Item Menu
    Also, added restart and quit buttons on pause screen

---
+++ Assets/Scripts/GUIScripts/MenuScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MenuScript : MonoBehaviour {
+
+	//public GameObject go;
+
+	//void Start(){
+
+	//	go = GameObject.Find ("Menu");
+	//}
+	public void Start_Button()
+	{
+	//	go.SetActive (false);
+		Application.LoadLevel ("CatPawScene");		
+			Debug.Log ("Hello");
+
+	}
+	public void Instr_Button()
+	{
+		//	go.SetActive (false);
+		Application.LoadLevel ("InstructionMenu");		
+		Debug.Log ("Hello");
+		
+	}
+}

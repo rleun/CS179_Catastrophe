7a12d1713b188264e96a83a96db10bbfc76456d1
raymond <weiwenliang1990@gmail.com>
2015-03-14 09:43:33 -0700

    Added chips - poison you or gain extra health

---
+++ Assets/Scripts/ChipsScript.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ChipsScript : MonoBehaviour {
+
+	GameObject Player;
+	PlayerHealth PlayerHealth;
+	int Number;
+	
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Player = GameObject.FindGameObjectWithTag ("Player");
+		PlayerHealth = Player.GetComponent<PlayerHealth> ();
+	}
+	
+	void OnTriggerEnter (Collider col)
+	{
+
+		if(col.name == "First Person Controller" || col.name == "cat_paw_animated")
+		{	
+			Number = Random.Range(1,10);
+			if(Number <= 6)
+			{
+				PlayerHealth.currentHealth = PlayerHealth.currentHealth-2;
+			}
+			else
+			{
+				PlayerHealth.currentHealth = PlayerHealth.currentHealth+2;
+			}
+			Destroy(gameObject);
+		}
+	}
+}
--- Assets/Scripts/HelicopterScripts/EffectsScript.cs
+++ Assets/Scripts/HelicopterScripts/EffectsScript.cs
@@ -4,12 +4,17 @@ using System.Collections;
 public class EffectsScript : MonoBehaviour {
 
 	public GameObject CustomExplosion;
+	public GameObject Burning;
+	GameObject Player;
+	EnemyHealth EnemyHealth;
+	bool IsBurning = false;
+
 	float CoutDown = 1f;
 	Vector3 position;
 
 	// Use this for initialization
 	void Start () {
-
+		EnemyHealth = GetComponent<EnemyHealth> ();
 	}
 	
 	// Update is called once per frame
@@ -18,7 +23,7 @@ public class EffectsScript : MonoBehaviour {
 		position = transform.position;
 		position.y = 4.7f;
 
-		if(CoutDown <= 0)
+		if(CoutDown <= 0 && EnemyHealth.currentHealth > 0)
 		{
 			GameObject explosion = Instantiate (CustomExplosion, transform.position, transform.rotation) as GameObject;
 			explosion.transform.position = position;
@@ -26,6 +31,15 @@ public class EffectsScript : MonoBehaviour {
 			explosion.rigidbody.AddForce(transform.forward * 10f);
 			Destroy (explosion, 4f);
 			CoutDown = 1f;
+		}
+
+		if(!IsBurning && EnemyHealth.currentHealth <= 0)
+		{
+			GameObject Burn = Instantiate (Burning, transform.position, transform.rotation) as GameObject;
+			position.x -= 4;
+			Burn.transform.position = position;
+			Debug.Log("Got here");
+			IsBurning = true;
 		}
 
 	}

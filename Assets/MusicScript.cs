d545691ff6765ee952908a773dab55117c1b1fe2
rleun <rleun005@gmail.com>
2015-03-16 01:03:10 -0700

    Instruction Scene fix

--- Assets/MusicScript.cs
+++ Assets/MusicScript.cs
@@ -3,13 +3,31 @@ using System.Collections;
 
 public class MusicScript : MonoBehaviour {
 
+	static bool created = false;
 	// Use this for initialization
 	void Start () {
 	
 	}
+
+	void Awake()
+	{
+		if(!created)
+		{
+			DontDestroyOnLoad (gameObject);
+			if(!gameObject.GetComponent<AudioSource>().isPlaying)
+			{
+				gameObject.GetComponent<AudioSource>().Play();
+			}
+			created = true;
+		}
+		else
+		{
+			Destroy(gameObject);
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		DontDestroyOnLoad (gameObject);
+
 	}
 }
